ba9049893562a1de329b11bdcbeda93d22eccce6
YiRanMushroom <yiranmushroom@gmail.com>
2024-06-12 22:02:41 +0800

    Learning

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,19 @@
 import dependencies.*
+import java.util.function.Function
 
-fun <T> Array<T>.changeEach(function: (T) -> T){
-    this.forEachIndexed{index, value -> this[index] = function(value)}
+fun <T> Array<T>.changeEach(function: (T) -> T) {
+    this.forEachIndexed { index, value -> this[index] = function(value) }
+}
+
+fun <R> invokeInt(`val`: Int, func: Function<Int, R>): R {
+    return func.apply(`val`)
 }
 
 fun main() {
+    var s1 = ""
+    var s2 = ""
+    s1 = readln()
+    s2 = readln()
+    if (s1 == s2) println("s1 and s2 are the same")
+    repeat(4) { println(it) }
 }
--- src/leetcodes/1002.kt
+++ src/leetcodes/1002.kt
@@ -4,16 +4,16 @@ import kotlin.math.min
 
 fun commonChars(words: Array<String>): List<String> {
     val map = Array(26) { 0 }
-    val lastMap = Array(26){Int.MAX_VALUE}
+    val lastMap = Array(26) { Int.MAX_VALUE }
     words.forEach {
-        it.forEach { map[it.code - 'a'.code]++}
-        lastMap.forEachIndexed{i, v -> lastMap[i] = min(lastMap[i],map[i])}
+        it.forEach { map[it.code - 'a'.code]++ }
+        lastMap.forEachIndexed { i, v -> lastMap[i] = min(lastMap[i], map[i]) }
         map.fill(0)
     }
 
     val arr = ArrayList<String>()
 
-    lastMap.forEachIndexed{i,v ->
+    lastMap.forEachIndexed { i, v ->
         repeat(v) {
             arr.add((i + 'a'.code).toChar().toString())
         }
